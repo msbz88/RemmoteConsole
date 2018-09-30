95d083c49af26a9bfc1e469393f844c35a21eebc
Hello World <8myspace8@gmail.com>
2018-09-30 11:56:00 +0300

    30/9/2018

--- RemmoteConsole/Program.cs
+++ RemmoteConsole/Program.cs
@@ -7,8 +7,11 @@ using System.Threading.Tasks;
 namespace RemmoteConsole {
     class Program {
         static void Main(string[] args) {
-            RemoteProccess.RunProcessOnRemoteMachine("", @"C:\Windows\system32\notepad.exe", @"", "");
+           RemoteProccess.RunProcessOnRemoteMachine("", @"C:\Windows\system32\calc.exe", "", "");
+           // RemoteProccess.PsExecRun("DK01WR5188", @"C:\Windows\system32\calc.exe", @"SCDOM\MSBZ", "Simcorp100");
+
             Console.WriteLine("Task ended.");
+            Console.ReadKey();
         }
     }
 }
--- RemmoteConsole/RemoteProccess.cs
+++ RemmoteConsole/RemoteProccess.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Management;
 using System.Text;
@@ -9,7 +10,6 @@ namespace RemmoteConsole {
     class RemoteProccess {
         public static void RunProcessOnRemoteMachine(string remoteMachine, string strPathToTheExe, string usernameAndDomain, string password) {
             try {
-                var processToRun = new[] { strPathToTheExe };
                 var connection = new ConnectionOptions();
                 connection.Impersonation = ImpersonationLevel.Impersonate;
                 connection.Authentication = AuthenticationLevel.Packet;
@@ -23,13 +23,42 @@ namespace RemmoteConsole {
                 var inParams = wmiProcess.GetMethodParameters("Create");
                 inParams["CommandLine"] = strPathToTheExe;
 
-                //InvokeMethodOptions methodOptions = new InvokeMethodOptions(null, TimeSpan.MaxValue);
                 var outParams = wmiProcess.InvokeMethod("Create", inParams, null);
 
                 Console.WriteLine("Process ID: " + outParams["processId"]);
 
             } catch (Exception ex) {
                 Console.WriteLine("Error occurred : " + ex.Message.ToString());
+            }
+        }
+
+        public static void PsExecRun(string remoteMachine, string strPathToTheExe, string usernameAndDomain, string password) {
+            Process p = new Process();
+            p.StartInfo.FileName = @"c:\PsTools\PsExec.exe";
+            p.StartInfo.Arguments = @"\\" + remoteMachine + " -u " + usernameAndDomain + " -p " + password + " -i " + strPathToTheExe;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.CreateNoWindow = true;
+            p.Start();
+        }
+
+        public void ListAllMachinesInLAN(string remoteMachine, string usernameAndDomain, string password) {
+            ConnectionOptions options = new ConnectionOptions();
+            options.Password = password;
+            options.Username = usernameAndDomain;
+
+            ManagementScope scope = new ManagementScope("\\\\"+ remoteMachine + "\\root\\cimv2", options);
+            scope.Connect();
+
+            //Query system for Operating System information
+            ObjectQuery query = new ObjectQuery("SELECT * FROM Win32_OperatingSystem");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+
+            ManagementObjectCollection queryCollection = searcher.Get();
+            foreach (ManagementObject m in queryCollection) {
+                // Display the remote computer information
+                Console.WriteLine("Computer Name : {0}", m["csname"]);
+                Console.WriteLine("Windows Directory : {0}", m["WindowsDirectory"]);
+                Console.WriteLine("Operating System: {0}", m["Caption"]);
             }
         }
         /*
